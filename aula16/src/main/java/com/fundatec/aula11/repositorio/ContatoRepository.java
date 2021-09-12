7f2fb32dd1f8bd860c449261385e8c80997bfddf
LucasFreitas19 <63620421+LucasFreitas19@users.noreply.github.com>
2021-09-12 00:32:54 -0300

    Delete aula16 directory

--- aula16/src/main/java/com/fundatec/aula11/repositorio/ContatoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fundatec.aula11.repositorio;
-
-import com.fundatec.aula11.dominio.Contato;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContatoRepository extends JpaRepository<Contato, Long>{
-}
--- aula16/src/main/java/com/fundatec/aula11/repositorio/HobbyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fundatec.aula11.repositorio;
-
-import com.fundatec.aula11.dominio.Hobby;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HobbyRepository extends JpaRepository<Hobby, Long> {
-}
